552da0cbce874af6acc70ed393781b82c5981266
Maaz Ahmad <60289705+MaazAhmad-tech@users.noreply.github.com>
2023-11-30 09:49:12 +0530

    Add files via upload
    Adding spring-core files

---
+++ Spring Course/02-spring-boot-spring-core/05-primary/src/main/java/com/springcourse/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,16 @@
+package com.springcourse.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*@SpringBootApplication(scanBasePackages = {"com.springcourse.springcoredemo",
+										   "com.springcourse.util"}
+)*/
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
---
+++ Spring Course/02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/springcourse/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,16 @@
+package com.springcourse.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+    private TennisCoach() {
+        System.out.println("In Constructor: "+ TennisCoach.class.getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice tennis for 30 mins!";
+    }
+}
